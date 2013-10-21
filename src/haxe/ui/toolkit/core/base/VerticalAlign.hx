1bbf8a37f43e9e72951104665c91526fb3e572d4
deep <system.grand@gmail.com>
2013-10-21 22:45:16 +0300

    align constants

---
+++ src/haxe/ui/toolkit/core/base/VerticalAlign.hx
@@ -0,0 +1,14 @@
+package haxe.ui.toolkit.core.base;
+
+/**
+ * ...
+ * @author deep <system.grand@gmail.com>
+ */
+class VerticalAlign
+{
+
+	static public inline var TOP:String = "top";
+	static public inline var BOTTOM:String = "bottom";
+	static public inline var CENTER:String = "center";
+	
+}
