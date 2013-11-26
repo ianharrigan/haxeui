a7794dc1c1bddf2e280d142f68cb0ac0cbd8e4f4
jalbanesi <jalbanesi@gmail.com>
2013-11-26 18:33:59 -0300

    Added support for embedded fonts via css: fontName: "assets/fonts/example.ttf";

--- src/haxe/ui/toolkit/core/Macros.hx
+++ src/haxe/ui/toolkit/core/Macros.hx
@@ -47,7 +47,7 @@ class Macros {
 				var props:Array<String> = styleData.split(":");
 				var propName:String = StringTools.trim(props[0]);
 				var propValue:String = StringTools.trim(props[1]);
-				if (ScriptUtils.isScript(propValue) && propName != "filter" && propName != "icon" && propName != "backgroundImage") {
+				if (ScriptUtils.isScript(propValue) && propName != "filter" && propName != "icon" && propName != "backgroundImage" && propName != "fontName") {
 					dynamicValues.set(propName, propValue);
 					continue;
 				}
