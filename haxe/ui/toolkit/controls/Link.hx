255f0a68a36eb260886e4e12d954bdf4dec3b75e
Ian Harrigan <ianharrigan@hotmail.com>
2014-12-01 09:33:47 +0100

    Added link component

---
+++ haxe/ui/toolkit/controls/Link.hx
@@ -0,0 +1,91 @@
+package haxe.ui.toolkit.controls;
+
+import haxe.ui.toolkit.controls.Text;
+import haxe.ui.toolkit.core.Component;
+import haxe.ui.toolkit.core.Screen;
+import haxe.ui.toolkit.events.UIEvent;
+import openfl.events.MouseEvent;
+import openfl.Lib;
+import openfl.net.URLRequest;
+
+class Link extends Text {
+	/**
+	 Link state is "normal" (default state)
+	 **/
+	public static inline var STATE_NORMAL = "normal";
+	/**
+	 Link state is "over"
+	 **/
+	public static inline var STATE_OVER = "over";
+	/**
+	 Link state is "down"
+	 **/
+	public static inline var STATE_DOWN = "down";
+	
+	private var _isDown:Bool = false;
+	private var _isOver:Bool = false;
+	
+	public function new() {
+		super();
+		useHandCursor = true;
+	}
+	
+	public override function initialize():Void {
+		super.initialize();
+		
+		// need to use screen as neko/cpp doesnt receive events unless background is solid colour
+		Screen.instance.addEventListener(MouseEvent.MOUSE_DOWN, _onScreenMouseDown);
+		Screen.instance.addEventListener(MouseEvent.MOUSE_MOVE, _onScreenMouseMove);
+	}
+	
+	private function _onScreenMouseDown(e:MouseEvent):Void {
+		if (hitTest(e.stageX, e.stageY) == true) {
+			_isDown = true;
+			state = STATE_DOWN;
+			Screen.instance.addEventListener(MouseEvent.MOUSE_UP, _onScreenMouseUp);
+		}
+	}
+
+	private function _onScreenMouseMove(e:MouseEvent):Void {
+		if (hitTest(e.stageX, e.stageY) == true) {
+			if (_isDown == true) {
+				state = STATE_DOWN;
+			} else {
+				state = STATE_OVER;
+			}
+			_isOver = true;
+		} else if (_isOver == true) {
+			state = STATE_NORMAL;
+			_isOver = false;
+		}
+	}
+	
+	private function _onScreenMouseUp(e:MouseEvent):Void {
+		Screen.instance.removeEventListener(MouseEvent.MOUSE_UP, _onScreenMouseUp);
+		if (hitTest(e.stageX, e.stageY) == true && _isDown == true) {
+			_isDown = false;
+			state = STATE_OVER;
+			
+			#if (!flash)
+			var uiEvent:UIEvent = new UIEvent(UIEvent.CLICK);
+			dispatchEvent(uiEvent);
+			#end
+			
+			if (url != null) {
+				Lib.getURL(new URLRequest(url));
+			}
+		} else if (_isOver == false) {
+			_isDown = false;
+			state = STATE_NORMAL;
+		}
+	}
+	
+	public var url(default, default):String;
+	
+	//******************************************************************************************
+	// IState
+	//******************************************************************************************
+	private override function get_states():Array<String> {
+		return [STATE_NORMAL, STATE_OVER, STATE_DOWN];
+	}
+}
