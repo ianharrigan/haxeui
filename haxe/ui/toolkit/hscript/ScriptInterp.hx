ef4cff79ff19073b7a3ea51fd036b6b245bcc60c
Ian Harrigan <ianharrigan@hotmail.com>
2014-12-21 13:51:25 +0100

    Fix for #194

--- haxe/ui/toolkit/hscript/ScriptInterp.hx
+++ haxe/ui/toolkit/hscript/ScriptInterp.hx
@@ -13,12 +13,12 @@ class ScriptInterp extends Interp {
 	}
 	
 	override function get( o : Dynamic, f : String ) : Dynamic {
-		if( o == null ) throw hscript.Expr.Error.EInvalidAccess(f);
+		if( o == null ) throw error(EInvalidAccess(f));
 		return Reflect.getProperty(o,f);
     }
 
     override function set( o : Dynamic, f : String, v : Dynamic ) : Dynamic {
-		if( o == null ) throw hscript.Expr.Error.EInvalidAccess(f);
+		if( o == null ) throw error(EInvalidAccess(f));
 		Reflect.setProperty(o,f,v);
 		return v;
     }
