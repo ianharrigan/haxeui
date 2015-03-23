1466d63fc6a43f328b9bb1027291989603c2d3fd
Ian Harrigan <ianharrigan@hotmail.com>
2015-03-23 22:26:22 +0100

    Fix for #197

--- haxe/ui/toolkit/controls/Image.hx
+++ haxe/ui/toolkit/controls/Image.hx
@@ -1,12 +1,21 @@
 package haxe.ui.toolkit.controls;
 
+import haxe.Http;
+import haxe.io.Bytes;
+import haxe.ui.toolkit.util.ByteConverter;
 import openfl.display.Bitmap;
 import openfl.display.BitmapData;
 import haxe.ui.toolkit.core.Component;
 import haxe.ui.toolkit.core.interfaces.IClonable;
 import haxe.ui.toolkit.core.interfaces.InvalidationFlag;
 import haxe.ui.toolkit.resources.ResourceManager;
+import openfl.display.Loader;
 import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.net.URLLoader;
+import openfl.net.URLLoaderDataFormat;
+import openfl.net.URLRequest;
+import openfl.utils.ByteArray;
 
 #if yagp
 import com.yagp.GifDecoder;
@@ -48,38 +57,18 @@ class Image extends Component implements IClonable<Image> {
 	//******************************************************************************************
 	private override function initialize():Void {
 		super.initialize();
-		
-		if (_bmp != null) {
-			//sprite.addChild(_bmp);
-			if (this.height > _bmp.height) {
-				//_bmp.y = Std.int((this.height / 2) - (_bmp.height / 2));
-			}
-			
-			if (autoSize == true) {
-				this.width = _bmp.bitmapData.width;
-				this.height = _bmp.bitmapData.height;
-			}
-		}
-		
-		#if yagp
-		if (_gifWrapper != null && autoSize == true) {
-			this.width = _gifWrapper.width;
-			this.height = _gifWrapper.height;
-		}
-		#end
-
-		#if svg
-		if (_svgSprite != null && autoSize == true) {
-			this.width = _svgSprite.width;
-			this.height = _svgSprite.height;
-		}
-		#end
+		updateContent();
 	}
 	
 	/**
 	 Destroy the image and free the resources (will be called by the framework automatically)
 	 **/
 	public override function dispose():Void {
+		disposeContent();
+		super.dispose();
+	}
+	
+	private function disposeContent():Void {
 		if (_bmp != null) {
 			if (_autoDisposeBitmapData == true) {
 				_bmp.bitmapData.dispose();
@@ -92,38 +81,23 @@ class Image extends Component implements IClonable<Image> {
 		if (_gifWrapper != null && sprite.contains(_gifWrapper)) {
 			_gifWrapper.dispose();
 			sprite.removeChild(_gifWrapper);
+			_gifWrapper = null;
 		}
 		#end
 		
 		#if svg
 		if (_svgSprite != null && sprite.contains(_svgSprite)) {
 			sprite.removeChild(_svgSprite);
+			_svgSprite = null;
 		}
 		#end
-		
-		super.dispose();
 	}
 	
 	public override function invalidate(type:Int = InvalidationFlag.ALL, recursive:Bool = false):Void {
 		super.invalidate(type, recursive);
 		
 		if (type & InvalidationFlag.SIZE == InvalidationFlag.SIZE) {
-			if (_stretch && _bmp != null) {
-				_bmp.width = width;
-				_bmp.height = height;
-			}
-			#if yagp
-			if (_stretch && _gifWrapper != null && sprite.contains(_gifWrapper)) {
-				_gifWrapper.width = width;
-				_gifWrapper.height = height;
-			}
-			#end
-			#if svg
-			if (_stretch && _svgSprite != null && sprite.contains(_svgSprite)) {
-				_svgSprite.width = width;
-				_svgSprite.height = height;
-			}
-			#end
+			updateContent();
 		}
 	}
 	
@@ -154,80 +128,161 @@ class Image extends Component implements IClonable<Image> {
 	}
 	
 	private function set_resource(value:Dynamic):Dynamic {
-		if (_bmp != null) {
-			if (_autoDisposeBitmapData == true) {
-				_bmp.bitmapData.dispose();
-			}
-			sprite.removeChild(_bmp);
-			_bmp = null;
-		}
+		disposeContent();
 		
-		#if yagp
-		if (_gifWrapper != null && sprite.contains(_gifWrapper)) {
-			_gifWrapper.dispose();
-			sprite.removeChild(_gifWrapper);
-		}
-		#end
-		
-		#if svg
-		if (_svgSprite != null && sprite.contains(_svgSprite)) {
-			sprite.removeChild(_svgSprite);
-		}
-		#end
-		
-		var bmpData:BitmapData = null;
 		if (Std.is(value, String)) {
-			var res:String = value;
-			if (StringTools.endsWith(res, ".gif")) {
+			if (StringTools.endsWith(value, ".gif")) {
 				#if yagp
-					var gif:Gif = GifDecoder.parseByteArray(ResourceManager.instance.getBytes(res));
-					var player:GifPlayer = new GifPlayer(gif);
-					_gifWrapper = new GifPlayerWrapper(player);
-					sprite.addChild(_gifWrapper);
-					if (autoSize == true && ready) {
-						this.width = _gifWrapper.width;
-						this.height = _gifWrapper.height;
-					}
+					loadGif(value, function(gif) {
+						updateGif(gif);
+					});
 				#else
 					trace("YAGP lib not found for .gif decoding");
 				#end
-			} else if (StringTools.endsWith(res, ".svg")) {
+			} else if (StringTools.endsWith(value, ".svg")) {
 				#if svg
-					var svg:SVG = ResourceManager.instance.getSVG(res);
-					_svgSprite = new Sprite();
-					//bmpData = new BitmapData(cast svg.data.width, cast svg.data.width);
-					//_bmp = new Bitmap(bmpData);
-					svg.render(_svgSprite.graphics);
-					_svgSprite.width = svg.data.width;
-					_svgSprite.height = svg.data.height;
-					sprite.addChild(_svgSprite);
-					if (autoSize == true && ready) {
-						this.width = _svgSprite.width;
-						this.height = _svgSprite.height;
-					}
+					loadSvg(value, function(svg) {
+						updateSvg(svg);
+					});
+				#else
+					trace("SVG lib not found for .svg decoding");
 				#end
 			} else {
-				bmpData = ResourceManager.instance.getBitmapData(res);
+				loadBitmap(value, function(bmpData) {
+					updateBitmap(bmpData);
+				});
 			}
 		} else if (Std.is(value, Bitmap)) {
-			bmpData = cast(value, Bitmap).bitmapData;
+			updateBitmap(cast(value, Bitmap).bitmapData);
 		} else if (Std.is(value, BitmapData)) {
-			bmpData = cast(value, BitmapData);
-		}
-		
-		if (bmpData != null) {
-			if (_bmp == null) {
-				_bmp = new Bitmap(bmpData);
-			}
-			sprite.addChild(_bmp);
-			if (autoSize == true && ready) {
-				this.width = _bmp.bitmapData.width;
-				this.height = _bmp.bitmapData.height;
-			}
+			updateBitmap(cast(value, BitmapData));
 		}
 		
 		_resource = value;
 		return value;
+	}
+	
+	#if yagp
+	private function loadGif(res:String, callback:Gif->Void):Void {
+		if (StringTools.startsWith(res, "http://")) {
+			var l:URLLoader = new URLLoader(); 
+			l.dataFormat = URLLoaderDataFormat.BINARY;
+			l.load(new URLRequest(res)); 
+			l.addEventListener(Event.COMPLETE, function(dyn) { 
+				callback(GifDecoder.parseByteArray(l.data));
+			});
+		} else {
+			callback(GifDecoder.parseByteArray(ResourceManager.instance.getBytes(res)));
+		}
+	}
+	
+	private function updateGif(gif:Gif):Void {
+		var player:GifPlayer = new GifPlayer(gif);
+		_gifWrapper = new GifPlayerWrapper(player);
+		updateContent();
+	}
+	#end
+	
+	#if svg
+	private function loadSvg(res:String, callback:SVG->Void):Void {
+		if (StringTools.startsWith(res, "http://")) {
+			var l:URLLoader = new URLLoader(); 
+			l.dataFormat = URLLoaderDataFormat.TEXT;
+			l.load(new URLRequest(res)); 
+			l.addEventListener(Event.COMPLETE, function(dyn) { 
+				callback(new SVG(l.data));
+			});
+		} else {
+			callback(ResourceManager.instance.getSVG(res));
+		}
+	}
+	
+	private function updateSvg(svg:SVG):Void {
+		_svgSprite = new Sprite();
+		svg.render(_svgSprite.graphics);
+		_svgSprite.width = svg.data.width;
+		_svgSprite.height = svg.data.height;
+		updateContent();
+	}
+	#end
+	
+	private function loadBitmap(res:String, callback:BitmapData->Void):Void {
+		if (StringTools.startsWith(res, "http://")) {
+			#if flash
+				var l:Loader = new Loader(); 
+				l.load(new URLRequest(res)); 
+				l.contentLoaderInfo.addEventListener(Event.INIT, function(dyn) { 
+					var bmp:Bitmap = cast(l.content, Bitmap);
+					var bmpData:BitmapData = new BitmapData(cast bmp.width, cast bmp.height, true, 0);
+					bmpData.draw(bmp.bitmapData);
+					callback(bmpData);
+				});
+			#else
+				var r:Http = new Http(res);
+				r.onData = function(imageData) {
+					callback(BitmapData.loadFromHaxeBytes(Bytes.ofString(imageData)));
+				}
+				r.request();
+			#end
+		} else {
+			callback(ResourceManager.instance.getBitmapData(res));
+		}
+	}
+	
+	private function updateBitmap(bmpData:BitmapData):Void {
+		_bmp = new Bitmap(bmpData);
+		updateContent();
+	}
+	
+	private function updateContent():Void {
+		#if yagp
+		if (_gifWrapper != null) {
+			if (sprite.contains(_gifWrapper) == false) {
+				sprite.addChild(_gifWrapper);
+			}
+			if (ready) {
+				if (autoSize == true) {
+					this.width = _gifWrapper.width;
+					this.height = _gifWrapper.height;
+				} else if (_stretch == true) {
+					_gifWrapper.width = width;
+					_gifWrapper.height = height;
+				}
+			}
+		}
+		#end
+
+		#if svg
+		if (_svgSprite != null) {
+			if (sprite.contains(_svgSprite) == false) {
+				sprite.addChild(_svgSprite);
+			}
+			if (ready) {
+				if (autoSize == true) {
+					this.width = _svgSprite.width;
+					this.height = _svgSprite.height;
+				} else if (_stretch == true) {
+					_svgSprite.width = width;
+					_svgSprite.height = height;
+				}
+			}
+		}
+		#end
+		
+		if (_bmp != null) {
+			if (sprite.contains(_bmp) == false) {
+				sprite.addChild(_bmp);
+			}
+			if (ready) {
+				if (autoSize == true) {
+					this.width = _bmp.bitmapData.width;
+					this.height = _bmp.bitmapData.height;
+				} else if (_stretch == true) {
+					_bmp.width = width;
+					_bmp.height = height;
+				}
+			}
+		}
 	}
 	
 	private function get_stretch():Bool {
