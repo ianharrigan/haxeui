f874fa311df0dcfdfea9f9a1586eec16f0cba4cc
Ian Harrigan <ianharrigan@hotmail.com>
2015-04-14 20:54:34 +0200

    Added text and binary resource hooks

--- haxe/ui/toolkit/resources/IResourceHook.hx
+++ haxe/ui/toolkit/resources/IResourceHook.hx
@@ -1,7 +1,10 @@
 package haxe.ui.toolkit.resources;
 
 import openfl.display.BitmapData;
+import openfl.utils.ByteArray;
 
 interface IResourceHook {
 	function getBitmapData(resourceId:String, locale:String = null):BitmapData;
+	function getText(resourceId:String, locale:String = null):String;
+	function getBytes(resourceId:String, locale:String = null):ByteArray;
 }
--- haxe/ui/toolkit/resources/ResourceManager.hx
+++ haxe/ui/toolkit/resources/ResourceManager.hx
@@ -44,7 +44,16 @@ class ResourceManager {
 	}
 	
 	public function getText(resourceId:String, locale:String = null):String {
-		var str:String = Resource.getString(resourceId);
+		var str:String = null;
+
+		if (resourceHook != null) {
+			str = resourceHook.getText(resourceId, locale);
+			if (str != null) {
+				return str;
+			}
+		}
+		
+		str = Resource.getString(resourceId);
 		if (str == null) {
 			str = Assets.getText(resourceId);
 		}
@@ -95,7 +104,17 @@ class ResourceManager {
 	}
 	
 	public function getBytes(resourceId:String, locale:String = null):ByteArray {
-		return Assets.getBytes(resourceId);
+		var ba:ByteArray = null;
+
+		if (resourceHook != null) {
+			ba = resourceHook.getBytes(resourceId, locale);
+			if (ba != null) {
+				return ba;
+			}
+		}
+		
+		ba = Assets.getBytes(resourceId);
+		return ba;
 	}
 	
 	public function reset():Void {
